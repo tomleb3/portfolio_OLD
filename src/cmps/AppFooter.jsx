b118c52a64fc2912a4b96be58b6c1e0b221899be
Tom Leb <tomleb3@gmail.com>
2021-04-15 15:55:52 +0300

    updated-cv-link

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -7,7 +7,7 @@ export const AppFooter = () => {
             <div className="social-container">
                 <a href="https://www.linkedin.com/in/tomleb/" target="_blank" rel="noopener noreferrer"><FiLinkedin className="social-link" /></a>
                 <a href="https://www.facebook.com/tomleb3/" target="_blank" rel="noopener noreferrer"><FiFacebook className="social-link" /></a>
-                <a href="https://github.com/tomleb3" target="_blank" rel="noopener noreferrer"><FiGithub className="social-link" /></a>
+                <a href="https://www.github.com/tomleb3" target="_blank" rel="noopener noreferrer"><FiGithub className="social-link" /></a>
             </div>
             <strong className="muted">&copy; 2021 | Tom Lebeodkin</strong>
             <small>Powered by React</small>
--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -6,8 +6,8 @@ export const AppHeader = () => {
         <section className="main-layout flex j-between a-center">
             <NavLink to="/"><img className="logo" src="https://res.cloudinary.com/tomleb3/image/upload/v1614691244/portfolio/logo_epjdqp.png" alt="" /></NavLink>
             <nav>
-                <a href="https://drive.google.com/file/d/1NbPQL4P1dtNDUsMx7qc3dQXWb44DTxp2/view?usp=sharing"
-                    activeClassName="active-link" target="_blank" rel="noopener noreferrer">Resume</a>
+                <a href="https://docs.google.com/document/d/1sX4zK-SDH9S6hxvfg227bkLnqSMpLCJOE9VXN8ONU2w/edit?usp=sharing"
+                    target="_blank" rel="noopener noreferrer">Resume</a>
                 <NavLink to="/contact" activeClassName="active-link">Contact</NavLink>
             </nav>
         </section>
